611122762d0bc711354a9213133655f08fd638f8
Moamen Ahmed <mmnmessed1@gmail.com>
2025-10-01 15:16:09 +0300

    Update QuizRepository to return Future<Either<Failure, List<Question>>> and dartz package is added

--- lib/features/quiz/data/repositories/quiz_repository.dart
+++ lib/features/quiz/data/repositories/quiz_repository.dart
@@ -1,31 +1,37 @@
+import 'dart:async';
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
+import 'package:quiz_app/features/quiz/data/models/failure.dart';
 import 'package:quiz_app/features/quiz/data/models/questions_model.dart';
 import 'package:http/http.dart' as http;
 
 class QuizRepository {
   static const String _baseUrl = 'https://opentdb.com/api.php';
 
-  Future<List<Question>> fetchQuestions({int amount = 10}) async {
+  Future<Either<Failure,List<Question>>> fetchQuestions({int amount = 10}) async {
     try {
       final response = await http.get(
         Uri.parse('$_baseUrl?amount=$amount&category=18&type=multiple'),
-      );
+      ).timeout(const Duration(seconds: 15));
 
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
         if (jsonData['response_code'] == 0) {
           final List<dynamic> results = jsonData['results'];
-          return results.map((json) => Question.fromApiJson(json)).toList();
+          final questions = results.map((json) => Question.fromApiJson(json)).toList();
+          return Right(questions);
           
         }else{
-          throw Exception('API error: ${jsonData['response_code']}');
+          return Left(Failure("Api error: ${jsonData['response_code']}: ${jsonData['response_message']}"));
         }
       }else{
-        throw Exception('Failed to load questions: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to load questions from API: $e');
+        return Left(Failure("Failed to load questions from API: ${response.statusCode}"));}
+    }on TimeoutException {
+      return Left(Failure('Request timed out. Please try again.'));
+    }
+     catch (e) {
+      return Left(Failure('Network error: $e'));
     }
   }
 }
