51c91097a249b1c43c0442bad43e59179cdf8635
Moamen Ahmed <mmnmessed1@gmail.com>
2025-10-01 15:31:00 +0300

    Update main.dart to provide the cubit with injected repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quiz_app/features/quiz/data/repositories/quiz_repository.dart';
 import 'package:quiz_app/features/quiz/presentation/cubit/quiz_cubit.dart';
 import 'package:quiz_app/features/quiz/presentation/pages/home_page.dart';
 import 'package:quiz_app/service_locator.dart';
@@ -16,7 +17,7 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => QuizCubit(),
+      create: (context) => QuizCubit(locator<QuizRepository>()),
       child:  MaterialApp(
           title: "Quiz App",
           debugShowCheckedModeBanner: false,
